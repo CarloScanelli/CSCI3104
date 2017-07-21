35f2567a03d045ea048dabe2162dd447cb068ff9
Carlo Scanelli <carloscanelli@engr2-2-84-58-dhcp.int.colorado.edu>
2017-07-20 22:02:04 -0600

    Removed useless files

--- Dijkstra/Dijkstra.py
+++ Dijkstra/Dijkstra.py
@@ -1,120 +1,121 @@
 import math
+from collections import defaultdict
+import operator
 
-# Function to read the textFile containing the information about
-# the graph.
+class Node:
+    def __init__(self):
+        self.id = None
+        self.x = None
+        self.y = None
+        self.neighbors = []
+        self.color = "white"
+        self.pi = None
+        self.adj = []
+
+class Graph:
+    def __init__(self):
+        self.nodes = []
+        self.edges = None
+        self.weights = None
+
+# Function to read the textFile containing the information about the graph
 def readTextFile():
-    txtFile = open("smallTest.txt","r") # open the textfile
+    # open the textfile
+    txtFile = open("smallTest.txt","r")
 
-    firstLine = txtFile.readline()  # reads first line of file
+    # reads first line of file, to extract number of vertices and edges
+    firstLine = txtFile.readline()
     vAnde = firstLine.split()
-
-    print "Number of vertices: ", vAnde[0]
+    print "\nNumber of vertices: ", vAnde[0]
     print "Number of edges: ", vAnde[1]
 
-    lines = txtFile.readlines() #read every line of file
+    # Read the rest of the file
+    lines = txtFile.readlines()
 
-    print "\nSection 1 - Nodes locations: "
-    firstSection = detectSection(lines, 1)  # find first section
+    # Find the first section, which holds the information about the locations
+    firstSection = detectSection(lines, 1)
 
-    # nodeIDs = []
-    # nodeX = []
-    # nodeY = []
-    # wombocombo = []
+    # Extract ids, x and y coords. Use these info to make an array of nodes.
+    # Sort the array, in case the nodes are not given in order in the txt file.
     infos = []
-    for i in firstSection:  # extract ids, x and y coords, and combine them
+    for i in firstSection:
         if (i != "\n"):
-            #print "i: ", i,
             info = i.split()
-            infos.append(info);
-    #         idDelimiter = detectWhiteSpace(i, 0)
-    #         print "idDelimiter: ", idDelimiter
-    #         nodeIDs.append(i[0 :idDelimiter])
-    #         xDelimiter = detectWhiteSpace(i, idDelimiter + 1)
-    #         print "xDelimiter: ", xDelimiter
-    #         nodeX.append(i[idDelimiter + 1 : xDelimiter])
-    #         yDelimiter = detectWhiteSpace(i, xDelimiter + 1)
-    #         nodeY.append(i[xDelimiter + 1 : yDelimiter])
-    #         wombocombo.append((i[0 :idDelimiter], i[idDelimiter + 1 : xDelimiter], i[xDelimiter + 1 : yDelimiter]))
-    # # print "Nodes IDs: ", nodeIDs
-    # print "X coords: ", nodeX
-    # print "Y coords: ", nodeY
-    print infos
+            infos.append(info)
+    nodes = buildNodes(infos)
+    nodes.sort(key=operator.attrgetter('id'))
 
+    # finds second section, which lists the connections
     firstSectionLength = len(firstSection);
-    print "\nSection 2 - connections: "
     secondSection = detectSection(lines, firstSectionLength + 1)
 
-    print findEdges(secondSection)
+    # get the connections/edges from the text file
+    connections = findEdges(secondSection)
 
+    # build the graph, and represent it as an adjacency list
+    g = edgesToDict(connections)
+    #print "\nGraph: ", g
+
+    # add the neighboring vertices to the node instances
+    for item in g:
+        nodes[int(item[0])].neighbors = map(int, item[1])
+
+    # build the graph
+    graph = buildGraph(nodes)
+
+    # find cycle
+    cycle = findCycle(graph)
+    print "Cycle? ", cycle
+
+    # finds the last section
     secondSectionLength = len(secondSection);
-    print "\nSection 3 - tests: "
+    # print "\nTests: "
     thirdSection = detectSection(lines, secondSectionLength + firstSectionLength + 1)
-    for i in thirdSection:
-        print i,
+    # for i in thirdSection:
+    #     print i,
+
     txtFile.close() # To free up any system resources taken up by the open file
 
+# creates an array of nodes, with respective ids and positions
+def buildNodes(nodesInfo):
+    nodes = []
+    for i in range(0, len(nodesInfo)):
+        newNode = Node()
+        newNode.id = int(nodesInfo[i][0])
+        newNode.x = int(nodesInfo[i][1:-1][0])
+        newNode.y = int(nodesInfo[i][2])
+        nodes.append(newNode)
+    return nodes
 
+# function to build the graph of nodes instances
+def buildGraph(nodes):
+    newGraph = Graph()
+    newGraph.nodes = nodes
+    return newGraph
+
+# Function to create a list which stores the connections
 def findEdges(connectionsList):
     connections = []
     for line in connectionsList:
         if (line != "\n"):
             parts = line.split()
-            #print parts
             connections.append((parts[0], parts[1]))
     return connections
 
+# Function to convert the list of connections to an adjacency list.
+# This is used to represent the graph.
+def edgesToDict(edges):
+    graphDict = defaultdict(list)
+    for k, v in edges:
+        graphDict[k].append(v)
+    return sorted(graphDict.items())
 
-    #{(1,2), (3),  }
-
-
-# def find_path(graph, start, end, path=[]):
-#     path = path + [start]
-#     if start == end:
-#         return path
-#     if not start not in graph:
-#         return None
-#     for node in graph:
-#         if node not in path[start]:
-#             newpath = find_path(graph, node, end, path)
-#             if newpath: return newpath
-#     return None
-    # testdict = {}
-    # keys = [0, 1]
-    # list1 = [1, [3, 4]]
-    # testdict[keys] = list1
-    # print "testdict :", testdict
-    #
-    # graphDict = {}
-    # # key = edges[0][0]
-    # # graphDict[key] = edges[0][1]
-    # # print "Dic: ", graphDict
-    # # print edges
-    # print "length: ", len(edges)
-    # startingNodes = []
-    # startingNodes.append(edges[0][0])
-    # for i in range(0, len(edges)):
-    #     if edges[i][0] not in startingNodes:
-    #         startingNodes.append(edges[i][0])
-    # key = startingNodes
-    # multiple = []
-    # for i in range(0, len(edges)):
-    #     if edges[i][0] in startingNodes:
-    #         multiple.append(edges[i][1:-1])
-    #
-    # graphDict[key] = multiple
-    # print "Dic: ", graphDict
-    # print "startingNodes: ", startingNodes
+# Function to find the weight of the edges, given the x and y coordinates
 def weightsFromCoords(X1, Y1, X2, Y2):
     weight = math.sqrt((X1 - X2)^2 + (Y1 - Y2)^2)
     return weight
 
-# Function to find a whitespace in a given line, starting at index start.
-# Retuns the index of the whitespace
-def detectWhiteSpace(line, start):
-    spaceIndex = line.find(" ", start);
-    return spaceIndex
-
-
+# Function to divide the textfile into sections
 def detectSection(file, start):
     section = []
     length = len(file)
@@ -123,5 +124,30 @@ def detectSection(file, start):
         if (file[i] == "\n"):
             break
     return section
+
+# Next two functions determine whether the graph has a cycle
+def findCycle(graph):
+    V = len(graph.nodes)
+    visited = [False] * V
+    path = [False] * V
+    for node in range(0, V):
+        if visited[node] == False:
+            if isCyclic(graph, node, visited, path) == True:
+                return True
+    return False
+
+def isCyclic(graph, index, visited, path):
+    visited[index] = True
+    path[index] = True
+
+    for i in graph.nodes[index].neighbors:
+        if visited[i] == False:
+            if isCyclic(graph, i, visited, path) == True:
+                return True
+        elif path[i] == True:
+            return True
+    path[index] = False
+    return False
+
 
 readTextFile()
